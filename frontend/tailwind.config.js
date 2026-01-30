c35bb38c6933b7ec9e1f63be35208336a68bdf65
alkorgli-B <alkorgli26@gmail.com>
2026-01-30 18:48:53 +0200

    Modify Tailwind config to add content paths
    Updated Tailwind CSS configuration to include additional content paths for file scanning.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // أضفنا هذه السطور لضمان قراءة الملفات حتى لو تغير المسار
+    "../app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
