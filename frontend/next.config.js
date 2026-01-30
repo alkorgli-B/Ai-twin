e6ecf8479f087fdb46a73cac7bf15caa4c882532
alkorgli-B <alkorgli26@gmail.com>
2026-01-30 17:55:47 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove 'export' for Replit (we need server)
-  images: {
-    unoptimized: true,
-  },
-  // Remove basePath for Replit
-}
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+export default nextConfig;
